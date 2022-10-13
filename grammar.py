cc22a563b6d6af02110c58786fb0096044e365f2
George Maia <georgemaiaf@gmail.com>
2022-10-13 20:09:28 -0300

    fix - table_ll1 com transação _

--- grammar.py
+++ grammar.py
@@ -1,3 +1,29 @@
+"""
+S -> 0A1 | B;
+A -> aA | _;
+B -> b;
+
+primeiros(S): {0, b}
+primeiros(A): {a, _}
+primeiros(B): {b}
+
+seguidores(S): {$}
+seguidores(A): {a, 1}
+seguidores(B): {$}
+"""
+table_ll1 = {
+  "S, 0": ["0", "A", "1"],
+  "S, b": ["B"], 
+  "A, 1": ["_"],
+  "A, a": ["a", "A"],
+  "B, b": ["b"]
+}
+
+"""
+S -> 0A | B;
+A -> aA | 1;
+B -> b;
+
 table_ll1 = {
   "S, 0": ["0", "A"],
   "S, b": ["B"], 
@@ -5,3 +31,4 @@ table_ll1 = {
   "A, a": ["a", "A"],
   "B, b": ["b"]
 }
+"""
